663383a6d64bea23fbd4bc449e3b627323bd89aa
EquinoxAlpha <93602271+EquinoxAlpha@users.noreply.github.com>
2023-05-02 01:28:25 +0200

    Fix settings reload crash

--- src/core/configuration.cpp
+++ src/core/configuration.cpp
@@ -1,5 +1,6 @@
 #include "configuration.h"
 
+#include <math.h>
 #include <thread>
 #include <sys/inotify.h>
 #include <unistd.h>
@@ -11,34 +12,36 @@ json settings;
 void reload(std::string path) {
     std::ifstream file(path);
     try {
-        file >> settings;
+        settings = json::parse(file);
+        logger::info("Reloaded config file at %s", path.c_str());
     } catch (json::parse_error &e) {
         logger::error("Failed to parse the config file");
-        return;
     }
+    file.close();
 }
 
 void init() {
-    // Init defaults
-    
-    // ...
-
-
     std::string home_dir = getpwuid(getuid())->pw_dir;
 
+    logger::info("Loading configuration from %s", (home_dir + "/ayypex_settings.json").c_str());
+
+    reload((home_dir + "/ayypex_settings.json").c_str());
+
     // Start watching for changes
-    std::thread inotify_thread([&home_dir]() {
+    std::thread inotify_thread([home_dir]() {
         int fd = inotify_init();
         if (fd < 0) {
             logger::error("Failed to initialize inotify");
             return;
         }
+        logger::info("Initialized inotify");
 
         int wd = inotify_add_watch(fd, (home_dir + "/ayypex_settings.json").c_str(), IN_MODIFY);
         if (wd < 0) {
             logger::error("Failed to add inotify watch");
             return;
         }
+        logger::info("Added inotify watch");
 
         char buffer[1024];
         while (true) {
@@ -49,7 +52,7 @@ void init() {
             }
 
             int i = 0;
-            while (i < length) {
+            while (i < length - 1) {
                 inotify_event *event = (inotify_event *)&buffer[i];
                 if (event->mask & IN_MODIFY) {
                     logger::info("Reloading configuration");
@@ -60,5 +63,6 @@ void init() {
         }
     });
     inotify_thread.detach();
+    logger::info("Started inotify thread");
 }
 } // namespace Configuration
